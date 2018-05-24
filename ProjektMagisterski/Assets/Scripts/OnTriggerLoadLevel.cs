7b431040c85d09088e055409eecf5e14d7b94240
viyanv <viyan.v@gmail.com>
2018-05-24 10:08:54 +0200

    Initial upload
    First commit containing
     + bedroom scene
     + office scene

---
+++ ProjektMagisterski/Assets/Scripts/OnTriggerLoadLevel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OnTriggerLoadLevel : MonoBehaviour {
+
+    [SerializeField] private string loadLevel;
+
+    void OnTriggerStay(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            if(Input.GetButtonDown("Use"))
+            {
+                SceneManager.LoadScene(loadLevel);
+            }
+        }
+    }
+
+}
