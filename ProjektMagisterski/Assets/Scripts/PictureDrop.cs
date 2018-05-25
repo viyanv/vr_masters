6cdc0c5d076ee461376659b5a97b9a85294fd765
viyanv <viyan.v@gmail.com>
2018-05-25 11:53:12 +0200

    VIVE integration
    Integrated application to work with HTC VIVE

---
+++ ProjektMagisterski/Assets/Scripts/PictureDrop.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PictureDrop : MonoBehaviour {
+
+    void OnCollisionEnter (Collision col)
+    {
+        if (col.gameObject.name == "PictureToDrop")
+        {
+            Rigidbody rb = col.gameObject.GetComponent<Rigidbody>();
+            rb.constraints = RigidbodyConstraints.None;
+            //Destroy(col.gameObject);
+            //Debug.Log(gameObject.name + " Triggered");
+        }
+    }
+
+}
